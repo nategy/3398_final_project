a0981cf5fadc7034e2f37d8ac0d389255878587c
nategy <grayjnason@gmail.com>
2022-11-28 19:36:14 -0600

    api setup

--- routes.py
+++ routes.py
@@ -1,7 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from dotenv import load_dotenv, find_dotenv
+import os
+import sys
+import requests
+import json
+
+load_dotenv(find_dotenv())
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+     YELP_API_KEY = os.getenv('YELP_API_KEY')
+    # print(YELP_API_KEY, file=sys.stderr)
+     YELP_API_BASE_URL = 'https://api.yelp.com/v3'
+     YELP_BUISNESS_URL = '/businesses/search'
+     YELP_BUISNESS_SEARCH_URL='https://api.yelp.com/v3/businesses/search?term=tacos&latitude=29.88889&longitude=-97.93889'
+     headers = {
+        'Authorization': f'Bearer {YELP_API_KEY}',
+        'Content-Type': 'application/json'
+     }
+     url_params = {'Authorization': f'Bearer {YELP_API_KEY}'}
+     response = requests.get(YELP_BUISNESS_SEARCH_URL, headers=headers, params=url_params)
+     data = response.json()
+     print(data, file=sys.stderr)
      return render_template("index.html")
+
+app.run(debug=True)
