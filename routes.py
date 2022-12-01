9ded9ef3ea0db2dce2cb8a4b68af1628a656d8e3
Holenhaugh <blafa365@gmail.com>
2022-11-30 20:36:01 -0600

    added additional json info getters

--- routes.py
+++ routes.py
@@ -9,6 +9,11 @@ load_dotenv(find_dotenv())
 
 app = Flask(__name__)
 
+arr_names = {}
+arr_urls = {}
+arr_ratings = {}
+
+
 @app.route('/')
 def home():
      YELP_API_KEY = os.getenv('YELP_API_KEY')
@@ -22,8 +27,20 @@ def home():
      }
      url_params = {'Authorization': f'Bearer {YELP_API_KEY}'}
      response = requests.get(YELP_BUISNESS_SEARCH_URL, headers=headers, params=url_params)
+
      data = response.json()
-     print(data, file=sys.stderr)
+     pretty_json_data = json.dumps(data, indent=4, sort_keys=True)
+     #print(pretty_json_data)
+
+     for index in range(20):   ###MAX IS 20 WE SHOULD PROB DO LESS THO
+      arr_names[index] = data['businesses'][index]['name']
+      arr_urls[index] = data['businesses'][index]['url']
+      arr_ratings[index] = data['businesses'][index]['rating']
+      print("\n")
+
+     for i in range(20):
+      print(arr_names[i] + arr_urls[i] + str(arr_ratings[i]))
+     
      return render_template("index.html")
 
 app.run(debug=True)
